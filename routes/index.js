41a786bb96d4f7f668fff2605b2d500e028df796
pabloarroyoib <pabloarroyoib@gmail.com>
2025-11-19 01:36:21 -0300

    cambio www, nodemon y ruta de products en app.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json({ message: 'Bienvenido a StarMink Store' });
 });
 
 module.exports = router;
